2c39f332c5839d9f3e9bdaccbe3c766fdad8fb35
yorkwongyuan <631824375@qq.com>
2018-08-07 10:35:05 +0800

    init

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import foo from './views/foo'
+import bar from './views/bar'
+const routes = {
+    '/foo':foo,
+    '/bar':bar
+}
+
+// Router类，用来控制页面根据URL切换
+class Router{
+   start(){
+       window.addEventListener('popstate',()=>{
+           this.load(location.pathname)
+       })
+       this.load(location.pathname)
+   }
+   // 前往 path，变更地址栏 URL，并加载相应页面
+   go(path){
+       // 变更地址栏 URL
+       history.pushState({},'',path)
+       // 加载页面
+       this.load(path);
+   }
+   load(path){
+        if(path === '/') path = '/foo'
+        // 创建页面实例
+        const view = new Routes[path]();
+        view.mount(document.body);
+   }
+}
+
+export default new Router()
---
+++ src/views/bar/index.js
@@ -0,0 +1,13 @@
+import router from '../../router'
+import template from './index.html'
+import './style.css'
+
+export default class{
+    mount(container){
+        document.title = 'bar'
+        container.innerHTML = template
+        container.querySelector(".bar__gofoo").addEventListener('click',()=>{
+            router.go('/foo')
+        })
+    }
+}
